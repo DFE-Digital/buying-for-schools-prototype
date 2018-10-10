527165f966cba729bcac3e2205b43843c38ab8ae
Chris Hanson <chrisphanson@outlook.com>
2018-10-10 10:39:26 +0100

    Added an option to set session data for prototype walkthroughs

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,41 @@ const marked = require('marked')
 // Set service name based on sub folders for different prototypes
 router.get('/create-specification*', function(req, res, next){
   res.locals['serviceName'] = 'Create a school purchase specification'
+
+  next()
+})
+
+
+
+
+
+// On start check prototype options
+router.get('/create-specification/start', function(req, res, next){
+  const options = req.session.data['prototype-options']
+
+  if (options == 'add-data') {
+    req.session.data['multiple-schools'] = 'No'
+    req.session.data['school-name'] = 'St Christopher\'s School'
+    req.session.data['school-address-line-1'] = 'Building A'
+    req.session.data['school-address-line-2'] = '123 School Road'
+    req.session.data['school-address-town'] = 'Schooltown'
+    req.session.data['school-address-county'] = 'South Schoolshire'
+    req.session.data['school-address-postcode'] = 'SK0 0LL'
+    req.session.data['school-pupils'] = '1000'
+    req.session.data['contract-type'] = 'One-off purchase'
+    req.session.data['purchase-date-day'] = '31'
+    req.session.data['purchase-date-month'] = '7'
+    req.session.data['purchase-date-year'] = '2019'
+    req.session.data['budget'] = '10000'
+    req.session.data['contract-title'] = 'We require a robust and enduring alarm system installing'
+    req.session.data['contract-description'] = 'Please provide quotes for the following options:\n\n1. Install a new wired alarm system with the required number of sensors, sounders and master panel\n2. Use the existing sounders and sensors but install a new master alarm panel\n3. Install a new wireless system with the required number of sensors, sounders and master panel.'
+    req.session.data['contact-name'] = 'Joe Bloggs'
+    req.session.data['contact-options'] = 'Email'
+    req.session.data['contact-email'] = 'joe@stchristophers.edu.uk'
+    req.session.data['response-type'] = 'Responses should be sent electronically and should include:\n\n1. Confirmation that you are able to offer the required service\n2. A full breakdown of fees\n3. 2 references from other schools'
+    req.session.data['response-assessment'] = 'Suppliers will be assessed against cost and their references.'
+  }
+
   next()
 })
 
@@ -15,10 +50,9 @@ router.get('/create-specification*', function(req, res, next){
 
 
 router.post('/create-specification/requirements/dates', function (req, res){
-
   const type = req.session.data['contract-type']
 
-  if (type == "One-off purchase") {
+  if (type == 'One-off purchase') {
     res.render('create-specification/requirements/purchase-date')
   } else {
     res.render('create-specification/requirements/service-dates')
