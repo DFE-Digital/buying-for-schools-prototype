45f373185a9bba3335c484bf1fa47cd8589c7eba
Chris Hanson <chrisphanson@outlook.com>
2018-11-15 11:24:20 +0000

    Made some changes after walkthrough with PO
    Service name change
    Fixed broken route for energy sub-category
    Updated OJEU content page
    Minor language tweaks

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Buying for schools',
+  serviceName: 'Buying goods or a service contract for your school',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -5,19 +5,23 @@ const marked = require('marked')
 // Add your routes here - above the module.exports line
 
 
-// Set service name
-// router.get('/service*', function(req, res, next){
-//   res.locals['serviceName'] = ''
-//
-//   next()
-// })
-
 // On start again delete session data
 router.get('/start-again', function (req, res) {
   req.session.destroy()
   res.redirect('/service/index')
 })
 
+// Check if catalogue
+router.get('/service/check-category', function (req, res) {
+  const category = req.session.data['category']
+
+  if (category == "Energy and utilities") {
+    res.redirect('/service/sub-category')
+  } else {
+    res.redirect('/service/method')
+  }
+
+})
 
 // Check if framework
 router.get('/service/check-method', function (req, res) {
